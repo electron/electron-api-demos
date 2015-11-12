91f8ef4b46e04f36091e97d09da93d1d4a704f50
Jessica Lord <jlord@github.com>
2015-11-12 11:46:43 +0000

    Start main.js

---
+++ main.js
@@ -0,0 +1,26 @@
+var app = require('app');
+var BrowserWindow = require('browser-window');
+var ipc = require('ipc');
+
+var mainWindow = null;
+
+var openFileDialog = require('./main-process/dialogs/open-file.js')
+openFileDialog()
+
+app.on('window-all-closed', () => {
+  if (process.platform != 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('ready', () => {
+  mainWindow = new BrowserWindow({ width: 800, height: 900 });
+
+  mainWindow.loadUrl('file://' + __dirname + '/index.html');
+
+  // mainWindow.openDevTools();
+
+  mainWindow.on('closed', () => {
+    mainWindow = null;
+  });
+});
