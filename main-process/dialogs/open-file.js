5d54707c2b008a8bf38f9f74c4151a338c4e98b4
Jessica Lord <jlord@github.com>
2015-11-12 11:48:00 +0000

    Main and Renderer process for open file

---
+++ main-process/dialogs/open-file.js
@@ -0,0 +1,19 @@
+var ipc = require('ipc');
+var dialog = require('dialog');
+var BrowserWindow = require('browser-window');
+
+module.exports = function OpenFileDialogMainProcess () {
+
+  ipc.on('open-file-dialog-sheet', function (event) {
+    var window = BrowserWindow.fromWebContents(event.sender)
+    console.log(event)
+    var files = dialog.showOpenDialog(window, { properties: [ 'openFile', 'openDirectory' ]})
+    if (files) { event.sender.send('selected-directory', files) }
+  });
+
+  ipc.on('open-file-dialog', function (event) {
+    var files = dialog.showOpenDialog({ properties: [ 'openFile', 'openDirectory' ]})
+    if (files) { event.sender.send('selected-directory', files) }
+  });
+
+}
---
+++ render-process/dialogs/open-file.js
@@ -0,0 +1,8 @@
+var selectDirBtn = document.getElementById('select-directory');
+var ipc = require('ipc');
+
+selectDirBtn.addEventListener('click', function clickedDir (event) {
+  ipc.send('open-file-dialog')
+  console.log('click')
+  // add sheet
+});
