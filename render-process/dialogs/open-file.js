e5ab33fad9ce592c948bdb48ed9a2cb0b1f967bd
Jessica Lord <jlord@github.com>
2015-12-01 19:55:13 -0800

    Add reply

--- render-process/dialogs/open-file.js
+++ render-process/dialogs/open-file.js
@@ -1,6 +1,10 @@
 var selectDirBtn = document.getElementById('select-directory');
 var ipc = require('electron').ipcRenderer;
 
-selectDirBtn.addEventListener('click', function clickedDir (event) {
+selectDirBtn.addEventListener('click', function (event) {
   ipc.send('open-file-dialog');
+});
+
+ipc.on('selected-directory', function (event, path) {
+  document.getElementById('selected-file').innerHTML = "You selected: " + path
 });
