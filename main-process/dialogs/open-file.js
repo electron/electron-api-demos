47aa34f8ae129232104bac6bd54a1f0b5095ead4
Jessica Lord <jlord@github.com>
2015-12-01 19:54:55 -0800

    :fire: unused example

--- main-process/dialogs/open-file.js
+++ main-process/dialogs/open-file.js
@@ -3,12 +3,6 @@ var dialog = require('dialog');
 var BrowserWindow = require('browser-window');
 
 module.exports = function OpenFileDialogMainProcess () {
-  ipc.on('open-file-dialog-sheet', function (event) {
-    var window = BrowserWindow.fromWebContents(event.sender);
-    var files = dialog.showOpenDialog(window, {properties: ['openFile', 'openDirectory']});
-    if (files) { event.sender.send('selected-directory', files); }
-  });
-
   ipc.on('open-file-dialog', function (event) {
     var files = dialog.showOpenDialog({properties: ['openFile', 'openDirectory']});
     if (files) { event.sender.send('selected-directory', files); }
