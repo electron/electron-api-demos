5a9a1454b1a2f60caab0ea4c3bf7ae0904a35745
Jessica Lord <jlord@github.com>
2016-01-05 14:50:45 -0800

    Add setup method

--- main-process/dialogs/open-file.js
+++ main-process/dialogs/open-file.js
@@ -1,7 +1,7 @@
 var ipc = require('electron').ipcMain;
 var dialog = require('dialog');
 
-module.exports = function OpenFileDialogMainProcess () {
+module.exports.setup = function () {
   ipc.on('open-file-dialog', function (event) {
     var files = dialog.showOpenDialog({properties: ['openFile', 'openDirectory']});
     if (files) { event.sender.send('selected-directory', files); }
