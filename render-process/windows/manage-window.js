8aada8041f390f60679b4da881dcad436d717a43
Jessica Lord <jlord@github.com>
2016-01-06 15:19:38 -0800

    Build out manage task

--- render-process/windows/manage-window.js
+++ render-process/windows/manage-window.js
@@ -1,13 +1,15 @@
-// var BrowserWindow = require('electron').remote.BrowserWindow;
-// var path = require('path');
-//
-// var newWindowBtn = document.getElementById('new-window');
-//
-// newWindowBtn.addEventListener('click', function (event) {
-//   var modalPath = 'file://' + path.join(process.cwd(), 'sections/windows/modal.html')
-//   var win = new BrowserWindow({ width: 400, height: 225 });
-//
-//   win.on('closed', function() { win = null; });
-//   win.loadURL(modalPath);
-//   win.show();
-// });
+var BrowserWindow = require('electron').remote.BrowserWindow;
+var path = require('path');
+
+var manageWindowBtn = document.getElementById('manage-window');
+
+manageWindowBtn.addEventListener('click', function (event) {
+  var modalPath = 'file://' + path.join(process.cwd(), 'sections/windows/manage-modal.html')
+  var win = new BrowserWindow({ width: 400, height: 225 });
+
+  win.on('move', function () { win.center() });
+  // TODO alter this window's content?
+  win.on('closed', function() { win = null; });
+  win.loadURL(modalPath);
+  win.show();
+});
