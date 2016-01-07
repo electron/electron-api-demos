f99f8dca32196a5d7f38c338414c7ce4aa49cf2d
Jessica Lord <jlord@github.com>
2016-01-06 16:34:05 -0800

    Add context menu task

---
+++ render-process/menus/context-menu.js
@@ -0,0 +1,20 @@
+var remote = require('electron').remote;
+var Menu = remote.Menu;
+var MenuItem = remote.MenuItem;
+
+var menu = new Menu();
+menu.append(new MenuItem({ label: 'Hello' }));
+menu.append(new MenuItem({ type: 'separator' }));
+menu.append(new MenuItem({ label: 'Electron' }));
+
+// When window is right clicked
+window.addEventListener('contextmenu', function (e) {
+  e.preventDefault();
+  menu.popup(remote.getCurrentWindow());
+}, false);
+
+// Display it when Demo button is clicked
+var contextMenuBtn = document.getElementById('context-menu');
+contextMenuBtn.addEventListener('click', function () {
+  menu.popup(remote.getCurrentWindow());
+});
