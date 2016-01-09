bb623d1e7071e9298b507f2bb4d3d1176efe11bd
Jessica Lord <jlord@github.com>
2016-01-08 16:13:11 -0800

    Explicit event name

--- render-process/communication/invisible-msg.js
+++ render-process/communication/invisible-msg.js
@@ -5,17 +5,16 @@ var path = require('path');
 
 var invisMsgBtn = document.getElementById('invis-msg');
 
-invisMsgBtn.addEventListener('click', function (event) {
-  console.log('click')
+invisMsgBtn.addEventListener('click', function (clickEvent) {
   var invisPath = 'file://' + path.join(process.cwd(), 'sections/communication/invisible.html')
   var win = new BrowserWindow({ width: 0, height: 0, show: false });
   win.loadURL(invisPath);
-  win.webContents.on('did-finish-load', function (event) {
+  win.webContents.on('did-finish-load', function (clickEvent) {
     // after invisible page loads, pass the original event
     // then use the main process browser window web contents get the window
     // from the event and then use webcontents to send a message that
     // the invisible window can listen for
-    var appWindow = BrowserWindow.fromWebContents(event.sender)
+    var appWindow = BrowserWindow.fromWebContents(clickEvent.sender)
     appWindow.webContents.send('send-ping', 'whoooooooh!');
   });
 });
