dfb3ae4b7aec611250287a672e9983413c2c5ac2
Jessica Lord <jlord@github.com>
2016-01-08 16:38:23 -0800

    Ok it didn't work, and this doesn't either

--- render-process/communication/invisible-msg.js
+++ render-process/communication/invisible-msg.js
@@ -6,16 +6,21 @@ var path = require('path');
 var invisMsgBtn = document.getElementById('invis-msg');
 
 invisMsgBtn.addEventListener('click', function (clickEvent) {
+  // var appWindow = BrowserWindow.fromWebContents(clickEvent.sender);
+  var appWindowID = BrowserWindow.getFocusedWindow().id
+
   var invisPath = 'file://' + path.join(process.cwd(), 'sections/communication/invisible.html')
-  var win = new BrowserWindow({ width: 0, height: 0, show: false });
+  var win = new BrowserWindow({ width: 400, height: 400, show: true });
   win.loadURL(invisPath);
+  console.log("win id:", win.id)
   win.webContents.on('did-finish-load', function (clickEvent) {
-    // after invisible page loads, pass the original event
-    // then use the main process browser window web contents get the window
-    // from the event and then use webcontents to send a message that
-    // the invisible window can listen for
-    var appWindow = BrowserWindow.fromWebContents(clickEvent.sender)
-    appWindow.webContents.send('send-ping', 'whoooooooh!');
+    // after invisible page loads, pass the original click event
+    // then use that to get the web contents from the main process browser window
+    // from there send a message that the invisible window can listen for
+    var appWindow = BrowserWindow.fromId(appWindowID)
+    console.log("appWindow id:", appWindowID)
+    console.log("title", appWindow.webContents.getTitle())
+    appWindow.webContents.send('send-ping', 'ping!');
   });
 });
 
