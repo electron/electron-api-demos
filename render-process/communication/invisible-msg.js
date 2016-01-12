1d22a348bb3b2ddcdaebc8c640e7770ccf8a17e2
Jessica Lord <jlord@github.com>
2016-01-12 15:59:05 -0800

    Invisible win comms task

--- render-process/communication/invisible-msg.js
+++ render-process/communication/invisible-msg.js
@@ -1,30 +1,24 @@
-var remote = require('electron').remote;
-var BrowserWindow = remote.BrowserWindow;
-var ipcMain = remote.ipcMain;
+var BrowserWindow = require('electron').remote.BrowserWindow;
+var ipcRenderer = require('electron').ipcRenderer
 var path = require('path');
 
 var invisMsgBtn = document.getElementById('invis-msg');
+var invisReply = document.getElementById('invis-reply');
 
 invisMsgBtn.addEventListener('click', function (clickEvent) {
-  // var appWindow = BrowserWindow.fromWebContents(clickEvent.sender);
-  var appWindowID = BrowserWindow.getFocusedWindow().id
+  var windowID = BrowserWindow.getFocusedWindow().id;
 
-  var invisPath = 'file://' + path.join(process.cwd(), 'sections/communication/invisible.html')
-  var win = new BrowserWindow({ width: 400, height: 400, show: true });
+  var invisPath = 'file://' + path.join(process.cwd(), 'sections/communication/invisible.html');
+  var win = new BrowserWindow({ width: 400, height: 400, show: false });
   win.loadURL(invisPath);
-  console.log("win id:", win.id)
+
   win.webContents.on('did-finish-load', function (clickEvent) {
-    // after invisible page loads, pass the original click event
-    // then use that to get the web contents from the main process browser window
-    // from there send a message that the invisible window can listen for
-    var appWindow = BrowserWindow.fromId(appWindowID)
-    console.log("appWindow id:", appWindowID)
-    console.log("title", appWindow.webContents.getTitle())
-    appWindow.webContents.send('send-ping', 'ping!');
+    var input = 100;
+    win.webContents.send('compute-factorial', input, windowID);
   });
 });
 
-ipcMain.on('ping-reply', function (event, arg) {
-  var message = "Invisible window reply: " + arg;
-  document.getElementById('invis-reply').innerHTML = message;
+ipcRenderer.on('factorial-computed', function (event, input, output) {
+  var message = 'The factorial of ' + input + ' is: ' + output
+  invisReply.textContent = message
 })
