bd2369c8b446c7532fdaa8f300ac363b26a4f70c
Jessica Lord <jlord@github.com>
2016-02-20 18:21:58 -0800

    Add back Edit menu
    Because it broke copy/paste completely

--- main-process/menus/application-menu.js
+++ main-process/menus/application-menu.js
@@ -4,6 +4,44 @@ var app = require('electron').app;
 module.exports.setup = function () {
   var template = [
     {
+      label: 'Edit',
+      submenu: [
+        {
+          label: 'Undo',
+          accelerator: 'CmdOrCtrl+Z',
+          role: 'undo'
+        },
+        {
+          label: 'Redo',
+          accelerator: 'Shift+CmdOrCtrl+Z',
+          role: 'redo'
+        },
+        {
+          type: 'separator'
+        },
+        {
+          label: 'Cut',
+          accelerator: 'CmdOrCtrl+X',
+          role: 'cut'
+        },
+        {
+          label: 'Copy',
+          accelerator: 'CmdOrCtrl+C',
+          role: 'copy'
+        },
+        {
+          label: 'Paste',
+          accelerator: 'CmdOrCtrl+V',
+          role: 'paste'
+        },
+        {
+          label: 'Select All',
+          accelerator: 'CmdOrCtrl+A',
+          role: 'selectall'
+        },
+      ]
+    },
+    { 
       label: 'View',
       submenu: [
         {
