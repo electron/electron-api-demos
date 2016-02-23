73413ef2d05e3c11af69accde4c9b8a5edc3e5a6
Jessica Lord <jlord@github.com>
2016-02-23 12:40:01 -0800

    Move import and nav scripts into files

---
+++ assets/imports.js
@@ -0,0 +1,10 @@
+var links = document.querySelectorAll('link[rel="import"]');
+var array = []
+// Import and add each page to the DOM
+array.forEach.call(links, function (link) {
+  var template = link.import.querySelector('.task-template')
+  var clone = document.importNode(template.content, true)
+  document.querySelector('.content').appendChild(clone);
+})
+// Run syntax highlighting
+hljs.initHighlightingOnLoad();
