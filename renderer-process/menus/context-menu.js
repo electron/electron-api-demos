195460b54ab2bdf0ce5afcf4f641502d769d7b98
Jessica Lord <jlord@github.com>
2016-02-25 13:54:22 -0800

    Menus edits

--- renderer-process/menus/context-menu.js
+++ renderer-process/menus/context-menu.js
@@ -7,13 +7,13 @@ menu.append(new MenuItem({ label: 'Hello' }));
 menu.append(new MenuItem({ type: 'separator' }));
 menu.append(new MenuItem({ label: 'Electron', type: 'checkbox', checked: true }));
 
-// When window is right-clicked
+// Show when window is right-clicked
 window.addEventListener('contextmenu', function (e) {
   e.preventDefault();
   menu.popup(remote.getCurrentWindow());
 }, false);
 
-// Display it when demo button is clicked
+// Show when demo button is clicked
 var contextMenuBtn = document.getElementById('context-menu');
 contextMenuBtn.addEventListener('click', function () {
   menu.popup(remote.getCurrentWindow());
