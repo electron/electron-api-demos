d082e62c8bc1f53662828d8778d1f19397b89e2c
Jessica Lord <jlord@github.com>
2016-02-24 16:27:49 -0800

    Replace var

--- assets/ex-links.js
+++ assets/ex-links.js
@@ -1,8 +1,7 @@
 var shell = require('shell');
 
 var links = document.querySelectorAll('a[href]');
-var array = [];
-array.forEach.call(links, function (link) {
+Array.prototype.forEach.call(links, function (link) {
   var url = link.getAttribute('href');
   if (url.indexOf('http') > -1) {
     link.addEventListener('click', function (e) {
