5b113616e76b5022bb626eb1dd5f0d3e920d733c
Jessica Lord <jlord@github.com>
2016-02-29 14:28:29 -0800

    Remove todos

--- main-process/menus/application-menu.js
+++ main-process/menus/application-menu.js
@@ -2,7 +2,6 @@ var Menu = require('electron').Menu;
 var app = require('electron').app;
 
 module.exports.setup = function () {
-  // TODO Windows accelerators?
   var template = [
     {
       label: 'Edit',
