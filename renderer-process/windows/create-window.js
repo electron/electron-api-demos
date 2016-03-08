db506258bfb81c6522e5effbb80a15ee6fe5b4cc
Jessica Lord <jlord@github.com>
2016-03-08 12:34:40 -0800

    Bump up create window size
    To fit on other OSes

--- renderer-process/windows/create-window.js
+++ renderer-process/windows/create-window.js
@@ -6,7 +6,7 @@ var newWindowBtn = document.getElementById('new-window');
 
 newWindowBtn.addEventListener('click', function (event) {
   var modalPath = 'file://' + path.join(process.cwd(), 'sections/windows/modal.html');
-  var win = new BrowserWindow({ width: 400, height: 225 });
+  var win = new BrowserWindow({ width: 400, height: 320 });
   win.on('closed', function () { win = null; });
   win.loadURL(modalPath);
   win.show();
