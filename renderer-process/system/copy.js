d03cc3e6f8520f19627ce191d55492dc0355012c
Jessica Lord <jlord@github.com>
2016-03-08 20:18:36 -0800

    Clear copy input if it has content

--- renderer-process/system/copy.js
+++ renderer-process/system/copy.js
@@ -4,6 +4,7 @@ var copyBtn = document.getElementById('copy-to');
 var copyInput = document.getElementById('copy-to-input');
 
 copyBtn.addEventListener('click', function () {
+  if (copyInput.value != '') copyInput.value = '';
   copyInput.placeholder = 'Copied! Paste here to see.';
   clipboard.writeText('Electron Demo!');
 });
