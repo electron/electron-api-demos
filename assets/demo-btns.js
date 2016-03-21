c651b9e47de4f1610599cfbf5170ecc2711f18c2
Zeke Sikelianos <zeke@sikelianos.com>
2016-03-21 13:44:05 -0700

    use classList.toggle()

--- assets/demo-btns.js
+++ assets/demo-btns.js
@@ -2,10 +2,6 @@ var demoBtns = document.querySelectorAll('.js-container-target');
 // Listen for demo button clicks
 Array.prototype.forEach.call(demoBtns, function (btn) {
   btn.addEventListener('click', function (event) {
-    if (event.target.parentElement.classList.contains('active')) {
-      event.target.parentElement.classList.remove('active');
-    } else {
-      event.target.parentElement.classList.add('active');
-    }
+    event.target.parentElement.classList.toggle('active');
   });
 });
