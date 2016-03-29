4dcfb7056f6ca25d6d3fa8ed4b156f0dd239307d
Zeke Sikelianos <zeke@sikelianos.com>
2016-03-29 13:33:24 -0700

    refactor main process scripts to use eval

--- main-process/menus/application-menu.js
+++ main-process/menus/application-menu.js
@@ -3,200 +3,165 @@ var BrowserWindow = electron.BrowserWindow
 var Menu = electron.Menu
 var app = electron.app
 
-module.exports.setup = function () {
-  var template = [
-    {
-      label: 'Edit',
-      submenu: [
-        {
-          label: 'Undo',
-          accelerator: 'CmdOrCtrl+Z',
-          role: 'undo'
-        },
-        {
-          label: 'Redo',
-          accelerator: 'Shift+CmdOrCtrl+Z',
-          role: 'redo'
-        },
-        {
-          type: 'separator'
-        },
-        {
-          label: 'Cut',
-          accelerator: 'CmdOrCtrl+X',
-          role: 'cut'
-        },
-        {
-          label: 'Copy',
-          accelerator: 'CmdOrCtrl+C',
-          role: 'copy'
-        },
-        {
-          label: 'Paste',
-          accelerator: 'CmdOrCtrl+V',
-          role: 'paste'
-        },
-        {
-          label: 'Select All',
-          accelerator: 'CmdOrCtrl+A',
-          role: 'selectall'
+var template = [{
+  label: 'Edit',
+  submenu: [{
+    label: 'Undo',
+    accelerator: 'CmdOrCtrl+Z',
+    role: 'undo'
+  }, {
+    label: 'Redo',
+    accelerator: 'Shift+CmdOrCtrl+Z',
+    role: 'redo'
+  }, {
+    type: 'separator'
+  }, {
+    label: 'Cut',
+    accelerator: 'CmdOrCtrl+X',
+    role: 'cut'
+  }, {
+    label: 'Copy',
+    accelerator: 'CmdOrCtrl+C',
+    role: 'copy'
+  }, {
+    label: 'Paste',
+    accelerator: 'CmdOrCtrl+V',
+    role: 'paste'
+  }, {
+    label: 'Select All',
+    accelerator: 'CmdOrCtrl+A',
+    role: 'selectall'
+  }]
+}, {
+  label: 'View',
+  submenu: [{
+    label: 'Reload',
+    accelerator: 'CmdOrCtrl+R',
+    click: function(item, focusedWindow) {
+      if (focusedWindow) {
+        // on reload, start fresh and close any old
+        // open secondary windows
+        if (focusedWindow.id === 1) {
+          BrowserWindow.getAllWindows().forEach(function(win) {
+            if (win.id > 1) {
+              win.close()
+            }
+          })
         }
-      ]
-    },
-    {
-      label: 'View',
-      submenu: [
-        {
-          label: 'Reload',
-          accelerator: 'CmdOrCtrl+R',
-          click: function (item, focusedWindow) {
-            if (focusedWindow) {
-              // on reload, start fresh and close any old
-              // open secondary windows
-              if (focusedWindow.id === 1) {
-                BrowserWindow.getAllWindows().forEach(function (win) {
-                  if (win.id > 1) {
-                    win.close()
-                  }
-                })
-              }
-              focusedWindow.reload()
-            }
-          }
-        },
-        {
-          label: 'Toggle Full Screen',
-          accelerator: (function () {
-            if (process.platform === 'darwin') {
-              return 'Ctrl+Command+F'
-            } else {
-              return 'F11'
-            }
-          })(),
-          click: function (item, focusedWindow) {
-            if (focusedWindow) {
-              focusedWindow.setFullScreen(!focusedWindow.isFullScreen())
-            }
-          }
-        },
-        {
-          label: 'Toggle Developer Tools',
-          accelerator: (function () {
-            if (process.platform === 'darwin') {
-              return 'Alt+Command+I'
-            } else {
-              return 'Ctrl+Shift+I'
-            }
-          })(),
-          click: function (item, focusedWindow) {
-            if (focusedWindow) {
-              focusedWindow.toggleDevTools()
-            }
-          }
-        },
-        {
-          type: 'separator'
-        },
-        {
-          label: 'App Menu Demo',
-          click: function (item, focusedWindow) {
-            if (focusedWindow) {
-              var options = {
-                type: 'info',
-                title: 'Application Menu Demo',
-                buttons: ['Ok'],
-                message: 'This demo is for the Menu section, showing how to create a clickable menu item in the application menu.'
-              }
-              electron.dialog.showMessageBox(focusedWindow, options, function () {})
-            }
-          }
-        }
-      ]
-    },
-    {
-      label: 'Window',
-      role: 'window',
-      submenu: [
-        {
-          label: 'Minimize',
-          accelerator: 'CmdOrCtrl+M',
-          role: 'minimize'
-        },
-        {
-          label: 'Close',
-          accelerator: 'CmdOrCtrl+W',
-          role: 'close'
-        }
-      ]
-    },
-    {
-      label: 'Help',
-      role: 'help',
-      submenu: [
-        {
-          label: 'Learn More',
-          click: function () { electron.shell.openExternal('http://electron.atom.io') }
-        }
-      ]
+        focusedWindow.reload()
+      }
     }
-  ]
-
-  if (process.platform === 'darwin') {
-    var name = electron.app.getName()
-    template.unshift({
-      label: name,
-      submenu: [
-        {
-          label: 'About ' + name,
-          role: 'about'
-        },
-        {
-          type: 'separator'
-        },
-        {
-          label: 'Services',
-          role: 'services',
-          submenu: []
-        },
-        {
-          type: 'separator'
-        },
-        {
-          label: 'Hide ' + name,
-          accelerator: 'Command+H',
-          role: 'hide'
-        },
-        {
-          label: 'Hide Others',
-          accelerator: 'Command+Shift+H',
-          role: 'hideothers'
-        },
-        {
-          label: 'Show All',
-          role: 'unhide'
-        },
-        {
-          type: 'separator'
-        },
-        {
-          label: 'Quit',
-          accelerator: 'Command+Q',
-          click: function () { app.quit() }
+  }, {
+    label: 'Toggle Full Screen',
+    accelerator: (function() {
+      if (process.platform === 'darwin') {
+        return 'Ctrl+Command+F'
+      } else {
+        return 'F11'
+      }
+    })(),
+    click: function(item, focusedWindow) {
+      if (focusedWindow) {
+        focusedWindow.setFullScreen(!focusedWindow.isFullScreen())
+      }
+    }
+  }, {
+    label: 'Toggle Developer Tools',
+    accelerator: (function() {
+      if (process.platform === 'darwin') {
+        return 'Alt+Command+I'
+      } else {
+        return 'Ctrl+Shift+I'
+      }
+    })(),
+    click: function(item, focusedWindow) {
+      if (focusedWindow) {
+        focusedWindow.toggleDevTools()
+      }
+    }
+  }, {
+    type: 'separator'
+  }, {
+    label: 'App Menu Demo',
+    click: function(item, focusedWindow) {
+      if (focusedWindow) {
+        var options = {
+          type: 'info',
+          title: 'Application Menu Demo',
+          buttons: ['Ok'],
+          message: 'This demo is for the Menu section, showing how to create a clickable menu item in the application menu.'
         }
-      ]
+        electron.dialog.showMessageBox(focusedWindow, options, function() {})
+      }
+    }
+  }]
+}, {
+  label: 'Window',
+  role: 'window',
+  submenu: [{
+    label: 'Minimize',
+    accelerator: 'CmdOrCtrl+M',
+    role: 'minimize'
+  }, {
+    label: 'Close',
+    accelerator: 'CmdOrCtrl+W',
+    role: 'close'
+  }]
+}, {
+  label: 'Help',
+  role: 'help',
+  submenu: [{
+    label: 'Learn More',
+    click: function() {
+      electron.shell.openExternal('http://electron.atom.io')
+    }
+  }]
+}]
+
+if (process.platform === 'darwin') {
+  var name = electron.app.getName()
+  template.unshift({
+      label: name,
+      submenu: [{
+        label: 'About ' + name,
+        role: 'about'
+      }, {
+        type: 'separator'
+      }, {
+        label: 'Services',
+        role: 'services',
+        submenu: []
+      }, {
+        type: 'separator'
+      }, {
+        label: 'Hide ' + name,
+        accelerator: 'Command+H',
+        role: 'hide'
+      }, {
+        label: 'Hide Others',
+        accelerator: 'Command+Shift+H',
+        role: 'hideothers'
+      }, {
+        label: 'Show All',
+        role: 'unhide'
+      }, {
+        type: 'separator'
+      }, {
+        label: 'Quit',
+        accelerator: 'Command+Q',
+        click: function() {
+          app.quit()
+        }
+      }]
     })
     // Window menu.
-    template[3].submenu.push(
-      {
-        type: 'separator'
-      },
-      {
-        label: 'Bring All to Front',
-        role: 'front'
-      }
-    )
-  }
-
-  var menu = Menu.buildFromTemplate(template)
-  Menu.setApplicationMenu(menu)
+  template[3].submenu.push({
+    type: 'separator'
+  }, {
+    label: 'Bring All to Front',
+    role: 'front'
+  })
 }
+
+var menu = Menu.buildFromTemplate(template)
+Menu.setApplicationMenu(menu)
