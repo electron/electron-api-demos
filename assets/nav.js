594b208f07b57bfc83598e6de694d2a67f7b1823
Zeke Sikelianos <zeke@sikelianos.com>
2016-03-29 16:24:34 -0700

    update nav to save last clicked button, and default to it on startup

--- assets/nav.js
+++ assets/nav.js
@@ -1,20 +1,31 @@
-// Listen for nav clicks
+const storage = require('electron-json-storage')
+
 document.body.addEventListener('click', function (event) {
-  if (classContains(event, 'nav-link') || classContains(event, 'nav-footer-link')) {
-    var viewId = '#' + event.target.dataset.view + '-view'
-    removeSelectedShow()
-    event.target.classList.add('is-selected')
-    document.querySelector(viewId).classList.add('show')
+  // Ignore clicks that don't have a target view
+  if (!event.target.dataset.view) return
+
+  // Hide currently active button and view
+  removeClass('is-selected')
+  removeClass('show')
+
+  // Highlight clicked button and show view
+  event.target.classList.add('is-selected')
+  const viewId = event.target.dataset.view + '-view'
+  document.getElementById(viewId).classList.add('show')
+
+  // Save currently active button in localStorage
+  storage.set('buttonId', event.target.getAttribute('id'))
+})
+
+// Default to the view that was active the last time the app was open
+storage.get('buttonId', function (err, buttonId) {
+  if (err) return console.error(err)
+  if (buttonId && buttonId.length) {
+    document.getElementById(buttonId).click()
   }
 })
 
-function classContains (event, className) {
-  return event.target.classList.contains(className)
-}
-
-function removeSelectedShow () {
-  if (document.querySelector('.is-selected')) {
-    document.querySelector('.is-selected').classList.remove('is-selected')
-  }
-  document.querySelector('.show').classList.remove('show')
+function removeClass (className) {
+  let el = document.querySelector('.' + className)
+  if (el) el.classList.remove(className)
 }
