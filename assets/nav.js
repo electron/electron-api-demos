9871a3be02746ff3ac88a6c3ea930b7405295bcd
Jessica Lord <jlord@github.com>
2016-04-05 14:56:29 -0700

    Start with the last demo viewed

--- assets/nav.js
+++ assets/nav.js
@@ -1,89 +1,82 @@
 var storage = require('electron-json-storage')
 
-storage.clear(function(error) {
-  if (error) throw error;
-  init()
-});
+// Default to the view that was active the last time the app was open
+storage.get('activeSectionButtonId', function (err, id) {
+  if (err) return console.error(err)
 
-function init () {
-
-  // Default to the view that was active the last time the app was open
-  storage.get('activeSectionButtonId', function (err, id) {
-    if (err) return console.error(err)
-
-    if (id && id.length) {
-      document.getElementById(id).click()
-    } else {
-      activateDefaultSection()
-      displayAbout()
-    }
-  })
-
-  document.body.addEventListener('click', function (event) {
-    if (event.target.dataset.section) {
-      handleSectionTrigger(event)
-    } else if (event.target.dataset.modal) {
-      handleModalTrigger(event)
-    } else if (event.target.classList.contains('modal-hide')) {
-      hideAllModals()
-    }
-  })
-
-  function handleSectionTrigger (event) {
-    hideAllSectionsAndDeselectButtons()
-
-    // Highlight clicked button and show view
-    event.target.classList.add('is-selected')
-
-    // Display the current section
-    let sectionId = event.target.dataset.section + '-section'
-    document.getElementById(sectionId).classList.add('show')
-
-    // Save currently active button in localStorage
-    let buttonId = event.target.getAttribute('id')
-    storage.set('activeSectionButtonId', buttonId, function (err) {
-      if (err) return console.error(err)
-    })
-  }
-
-  function activateDefaultSection () {
-    document.getElementById('button-windows').click()
-  }
-
-  function showMainContent () {
-    document.querySelector('.section-nav').classList.add('show')
-    document.querySelector('.content').classList.add('show')
-  }
-
-  function handleModalTrigger (event) {
-    hideAllModals()
-    let modalId = event.target.dataset.modal + '-modal'
-    console.log('modalId', modalId)
-    document.getElementById(modalId).classList.add('show')
-  }
-
-  function hideAllModals () {
-    let modals = document.querySelectorAll('.modal.show')
-    Array.prototype.forEach.call(modals, function (modal) {
-      modal.classList.remove('show')
-    })
+  if (id && id.length) {
     showMainContent()
+    document.getElementById(id).click()
+  } else {
+    activateDefaultSection()
+    displayAbout()
   }
+})
 
-  function hideAllSectionsAndDeselectButtons () {
-
-    let sections = document.querySelectorAll('.content-item.show')
-    Array.prototype.forEach.call(sections, function (section) {
-      section.classList.remove('show')
-    })
-
-    let buttons = document.querySelectorAll('.nav-link.is-selected')
-    Array.prototype.forEach.call(buttons, function (button) {
-      button.classList.remove('is-selected')
-    })
+document.body.addEventListener('click', function (event) {
+  if (event.target.dataset.section) {
+    handleSectionTrigger(event)
+  } else if (event.target.dataset.modal) {
+    handleModalTrigger(event)
+  } else if (event.target.classList.contains('modal-hide')) {
+    hideAllModals()
   }
+})
 
-  function displayAbout () {
-    document.querySelector('#about-modal').classList.add('show')
-  }
+function handleSectionTrigger (event) {
+  hideAllSectionsAndDeselectButtons()
+
+  // Highlight clicked button and show view
+  event.target.classList.add('is-selected')
+
+  // Display the current section
+  let sectionId = event.target.dataset.section + '-section'
+  document.getElementById(sectionId).classList.add('show')
+
+  // Save currently active button in localStorage
+  let buttonId = event.target.getAttribute('id')
+  storage.set('activeSectionButtonId', buttonId, function (err) {
+    if (err) return console.error(err)
+  })
+}
+
+function activateDefaultSection () {
+  document.getElementById('button-windows').click()
+}
+
+function showMainContent () {
+  document.querySelector('.section-nav').classList.add('show')
+  document.querySelector('.content').classList.add('show')
+}
+
+function handleModalTrigger (event) {
+  hideAllModals()
+  let modalId = event.target.dataset.modal + '-modal'
+  console.log('modalId', modalId)
+  document.getElementById(modalId).classList.add('show')
+}
+
+function hideAllModals () {
+  let modals = document.querySelectorAll('.modal.show')
+  Array.prototype.forEach.call(modals, function (modal) {
+    modal.classList.remove('show')
+  })
+  showMainContent()
+}
+
+function hideAllSectionsAndDeselectButtons () {
+
+  let sections = document.querySelectorAll('.content-item.show')
+  Array.prototype.forEach.call(sections, function (section) {
+    section.classList.remove('show')
+  })
+
+  let buttons = document.querySelectorAll('.nav-link.is-selected')
+  Array.prototype.forEach.call(buttons, function (button) {
+    button.classList.remove('is-selected')
+  })
+}
+
+function displayAbout () {
+  document.querySelector('#about-modal').classList.add('show')
 }
