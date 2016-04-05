a7b28e33fb018b78516cb30e886e072239ac2736
Jessica Lord <jlord@github.com>
2016-04-05 16:43:55 -0700

    Linted

--- main-process/native-ui/dialogs/open-file.js
+++ main-process/native-ui/dialogs/open-file.js
@@ -1,10 +1,10 @@
 var ipc = require('electron').ipcMain
 var dialog = require('electron').dialog
 
-ipc.on('open-file-dialog', function(event) {
+ipc.on('open-file-dialog', function (event) {
   dialog.showOpenDialog({
     properties: ['openFile', 'openDirectory']
-  }, function(files) {
+  }, function (files) {
     if (files) event.sender.send('selected-directory', files)
   })
 })
