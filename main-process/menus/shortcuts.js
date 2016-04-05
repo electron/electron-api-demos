6685a31b552101974e93306a4ea0c827dac818d0
Zeke Sikelianos <zeke@sikelianos.com>
2016-04-05 13:49:25 -0700

    shortcut stuff

---
+++ main-process/menus/shortcuts.js
@@ -0,0 +1,16 @@
+var electron = require('electron')
+var app = electron.app
+var globalShortcut = electron.globalShortcut
+
+// app.on('ready', function () {
+//   let shorty = globalShortcut.register('D', function () {
+//     console.log('D is pressed')
+//   })
+//
+//   console.log('shorty')
+//   console.log(shorty)
+// })
+//
+// app.on('will-quit', function () {
+//   globalShortcut.unregisterAll()
+// })
