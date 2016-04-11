53043a963e573a9c58191da21bf11f1a31cc93a1
Kevin Sawicki <kevinsawicki@gmail.com>
2016-04-11 15:10:45 -0700

    Delete installer output folder before building it

--- script/installer.js
+++ script/installer.js
@@ -2,10 +2,13 @@
 
 const createWindowsInstaller = require('electron-winstaller').createWindowsInstaller
 const path = require('path')
+const rimraf = require('rimraf')
 
-getInstallerConfig()
+deleteOutputFolder()
+  .then(getInstallerConfig)
   .then(createWindowsInstaller)
   .catch((error) => {
+    console.log(error.stack);
     console.error(error.message || error)
     process.exit(1)
   })
@@ -16,5 +19,14 @@ function getInstallerConfig () {
     appDirectory: path.join(outPath, 'Electron API Demos-win32-ia32'),
     exe: 'Electron API Demos.exe',
     outputDirectory: path.join(outPath, 'windows-installer')
+  })
+}
+
+function deleteOutputFolder () {
+  return new Promise((resolve, reject) => {
+    rimraf(path.join(__dirname, '..', 'out', 'windows-installer'), (error) => {
+      if (error) return reject(error)
+      resolve()
+    })
   })
 }
