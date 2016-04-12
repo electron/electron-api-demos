9a2848eb41a4e5d465438a4123834e9abc3fe67a
Kevin Sawicki <kevinsawicki@gmail.com>
2016-04-12 12:44:06 -0700

    Create initialize function to setup app

--- main.js
+++ main.js
@@ -3,10 +3,44 @@ const glob = require('glob')
 const electron = require('electron')
 const BrowserWindow = electron.BrowserWindow
 var app = electron.app
-var mainWindow = null
 
 electron.hideInternalModules()
 process.throwDeprecation = true
+
+var mainWindow = null
+
+function initialize () {
+  // Require and setup each JS file in the main-process dir
+  glob(path.join(__dirname, 'main-process/**/*.js'), function (error, files) {
+    if (error) return console.log(error)
+    files.forEach(function (file) {
+      require(file)
+    })
+  })
+
+  function createWindow () {
+    var iconPath = path.join(__dirname, '/assets/app-icon/png/512.png')
+    mainWindow = new BrowserWindow({ width: 1080, minWidth: 680, height: 800, icon: iconPath })
+    mainWindow.loadURL('file://' + path.join(__dirname, 'index.html'))
+    mainWindow.on('closed', function () {
+      mainWindow = null
+    })
+  }
+
+  app.on('ready', createWindow)
+
+  app.on('window-all-closed', function () {
+    if (process.platform !== 'darwin') {
+      app.quit()
+    }
+  })
+
+  app.on('activate', function () {
+    if (mainWindow === null) {
+      createWindow()
+    }
+  })
+}
 
 // Handle Squirrel on Windows startup events
 switch (process.argv[1]) {
@@ -14,36 +48,8 @@ switch (process.argv[1]) {
   case '--squirrel-obsolete':
   case '--squirrel-updated':
   case '--squirrel-uninstall':
-    return app.quit()
-}
-
-// Require and setup each JS file in the main-process dir
-glob(path.join(__dirname, 'main-process/**/*.js'), function (error, files) {
-  if (error) return console.log(error)
-  files.forEach(function (file) {
-    require(file)
-  })
-})
-
-function createWindow () {
-  var iconPath = path.join(__dirname, '/assets/app-icon/png/512.png')
-  mainWindow = new BrowserWindow({ width: 1080, minWidth: 680, height: 800, icon: iconPath })
-  mainWindow.loadURL('file://' + path.join(__dirname, 'index.html'))
-  mainWindow.on('closed', function () {
-    mainWindow = null
-  })
-}
-
-app.on('ready', createWindow)
-
-app.on('window-all-closed', function () {
-  if (process.platform !== 'darwin') {
     app.quit()
-  }
-})
-
-app.on('activate', function () {
-  if (mainWindow === null) {
-    createWindow()
-  }
-})
+    break
+  default:
+    initialize()
+}
