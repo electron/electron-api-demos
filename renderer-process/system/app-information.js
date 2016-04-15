20f1b1b95c387674293e4dfd223eedf7e5f83b24
Jessica Lord <jlord@github.com>
2016-04-15 16:28:40 -0700

    Add app path demo

---
+++ renderer-process/system/app-information.js
@@ -0,0 +1,12 @@
+const ipc = require('electron').ipcRenderer
+
+const appInfoBtn = document.getElementById('app-info')
+
+appInfoBtn.addEventListener('click', function () {
+  ipc.send('get-app-path')
+})
+
+ipc.on('got-app-path', function (event, path) {
+  let message = `This app is located at: ${path}`
+  document.getElementById('got-app-info').innerHTML = message
+})
