c77b547e12d05dbe7f182bdf4492354fd696284a
Jessica Lord <jlord@github.com>
2016-04-19 15:50:38 -0700

    Show dialog when user returns to app

--- main-process/system/protocol-handler.js
+++ main-process/system/protocol-handler.js
@@ -1,3 +1,8 @@
 const app = require('electron').app
+const dialog = require('electron').dialog
 
 app.setAsDefaultProtocolClient('electron-api-demos')
+
+app.on('open-url', function (event, url) {
+  dialog.showErrorBox('Welcome Back', `You arrived from: ${url}` )
+})
