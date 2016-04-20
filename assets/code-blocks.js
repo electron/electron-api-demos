625691bd45a5a47700528cb1666306afd9aa52e4
Kevin Sawicki <kevinsawicki@gmail.com>
2016-04-20 08:40:41 -0700

    Highlight all pre code blocks

--- assets/code-blocks.js
+++ assets/code-blocks.js
@@ -2,9 +2,13 @@ var fs = require('fs')
 var highlight = require('highlight.js')
 var path = require('path')
 
-var codeBlocks = document.querySelectorAll('code[data-path]')
-Array.prototype.forEach.call(codeBlocks, function (code) {
+var codeBlocksWithPaths = document.querySelectorAll('code[data-path]')
+Array.prototype.forEach.call(codeBlocksWithPaths, function (code) {
   var codePath = code.dataset.path
   code.textContent = fs.readFileSync(path.join(__dirname, '..', codePath))
+})
+
+var codeBlocks = document.querySelectorAll('pre code')
+Array.prototype.forEach.call(codeBlocks, function (code) {
   highlight.highlightBlock(code)
 })
