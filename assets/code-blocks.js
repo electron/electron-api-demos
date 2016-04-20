c2d20c913c3c612593c0e4fb81616a51e7a3735c
Kevin Sawicki <kevinsawicki@gmail.com>
2016-04-20 08:36:41 -0700

    Highlight each code block using lib

--- assets/code-blocks.js
+++ assets/code-blocks.js
@@ -1,8 +1,10 @@
 var fs = require('fs')
+var highlight = require('highlight.js')
 var path = require('path')
 
 var codeBlocks = document.querySelectorAll('code[data-path]')
 Array.prototype.forEach.call(codeBlocks, function (code) {
   var codePath = code.dataset.path
   code.textContent = fs.readFileSync(path.join(__dirname, '..', codePath))
+  highlight.highlightBlock(code)
 })
