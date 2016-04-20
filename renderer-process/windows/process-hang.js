7c1d2d8742eb068cd3351917314299b854e63427
Jessica Lord <jlord@github.com>
2016-04-19 21:30:09 -0700

    Not working - Add process hang demo

---
+++ renderer-process/windows/process-hang.js
@@ -0,0 +1,29 @@
+var BrowserWindow = require('electron').remote.BrowserWindow
+var dialog = require('electron').remote.dialog
+
+var path = require('path')
+
+var processHangBtn = document.getElementById('process-hang')
+
+processHangBtn.addEventListener('click', function (event) {
+  var hangWinPath = path.join('file://', __dirname, '../../sections/windows/process-hang.html')
+  var win = new BrowserWindow({ width: 400, height: 320 })
+
+  win.webContents.on('unresponsive', function () {
+    console.log('unresponsive')
+    var options = {
+      type: 'info',
+      title: 'Renderer Process Hanging',
+      message: "This process is hanging.",
+      buttons: ['Reload', 'Close']
+    }
+    dialog.showMessageBox(options, function (index) {
+      if (index === 0) win.reload()
+      else win.close()
+    })
+  })
+
+  win.on('closed', function () { win = null })
+  win.loadURL(hangWinPath)
+  win.show()
+})
