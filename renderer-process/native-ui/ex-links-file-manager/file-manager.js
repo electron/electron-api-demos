2028f1c8a54da25e857e05f83ed50ffedbdd7e3c
Benjiko99 <benjiko99@gmail.com>
2016-05-17 16:54:04 +0200

    Corrected a typo #5

--- renderer-process/native-ui/ex-links-file-manager/file-manager.js
+++ renderer-process/native-ui/ex-links-file-manager/file-manager.js
@@ -2,8 +2,8 @@ const shell = require('electron').shell
 
 const os = require('os')
 
-const fileMangerBtn = document.getElementById('open-file-manager')
+const fileManagerBtn = document.getElementById('open-file-manager')
 
-fileMangerBtn.addEventListener('click', function (event) {
+fileManagerBtn.addEventListener('click', function (event) {
   shell.showItemInFolder(os.homedir())
 })
