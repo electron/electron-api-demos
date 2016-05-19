39dedbb6e8a412bc4107ffdf6f9cc8067226c73e
Kevin Sawicki <kevinsawicki@gmail.com>
2016-05-19 15:02:41 -0700

    Use Cmd+Alt+K instead of D

--- main-process/menus/shortcuts.js
+++ main-process/menus/shortcuts.js
@@ -4,7 +4,7 @@ const dialog = electron.dialog
 const globalShortcut = electron.globalShortcut
 
 app.on('ready', function () {
-  globalShortcut.register('CommandOrControl+Alt+D', function () {
+  globalShortcut.register('CommandOrControl+Alt+K', function () {
     dialog.showMessageBox({
       type: 'info',
       message: 'Success! You pressed the registered global shortcut keybinding.',
