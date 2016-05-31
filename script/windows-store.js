1f9adfa77a91b2d99ea156e93197ae45240fdc14
Kevin Sawicki <kevinsawicki@gmail.com>
2016-05-31 10:07:04 -0700

    Add initial windows-store script

---
+++ script/windows-store.js
@@ -0,0 +1,23 @@
+const ChildProcess = require('child_process')
+const path = require('path')
+
+const version = require('../package').version
+
+const command = path.join(__dirname, '..', 'node_modules', '.bin', 'electron-windows-store.cmd')
+const args = [
+  '--input-directory',
+  path.join(__dirname, '..', 'ElectronAPIDemos-win32-ia32'),
+  '--output-directory',
+  path.join(__dirname, '..', 'windows-store'),
+  '--flatten',
+  true,
+  '--package-version',
+  version,
+  '--package-name',
+  'ElectronAPIDemos'
+]
+
+const windowsStore = ChildProcess.spawn(command, args, {stdio: 'inherit'})
+windowsStore.on('close', (code) => {
+  process.exit(code)
+})
