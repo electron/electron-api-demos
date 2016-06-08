697cbda912c88ff911027ee8298fba8fbc728ce8
Jessica Lord <jlord@github.com>
2016-06-08 13:24:44 -0700

    Use .size for demo

--- renderer-process/system/screen-information.js
+++ renderer-process/system/screen-information.js
@@ -1,7 +1,7 @@
 const electronScreen = require('electron').screen
 
 const screenInfoBtn = document.getElementById('screen-info')
-const size = electronScreen.getPrimaryDisplay().workAreaSize
+const size = electronScreen.getPrimaryDisplay().size
 
 screenInfoBtn.addEventListener('click', function () {
   const message = `Your screen is: ${size.width} px x ${size.height} px`
