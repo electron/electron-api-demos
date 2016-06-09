eb6077e046c6a4b6d3d4a2ae58706d906ac67244
Kevin Sawicki <kevinsawicki@gmail.com>
2016-06-09 16:05:13 -0700

    Prepare next review build

--- script/mas.sh
+++ script/mas.sh
@@ -12,7 +12,7 @@ electron-packager . \
   --platform=mas \
   --app-bundle-id=com.github.electron-api-demos \
   --app-version="$npm_package_version" \
-  --build-version="1.0.3" \
+  --build-version="1.0.4" \
   --arch=x64 \
   --icon=assets/app-icon/mac/app.icns \
   --prune=true \
