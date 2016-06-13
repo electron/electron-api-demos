3b4a3659f92ac56b7fe5dc093150804182402a63
Kevin Sawicki <kevinsawicki@gmail.com>
2016-06-13 11:24:15 -0700

    Unpack protocol-link.html

--- renderer-process/system/protocol-handler.js
+++ renderer-process/system/protocol-handler.js
@@ -5,6 +5,7 @@ const path = require('path')
 const protocolHandlerBtn = document.getElementById('protocol-handler')
 
 protocolHandlerBtn.addEventListener('click', function () {
-  const pagePath = path.join('file://', __dirname, '../../sections/system/protocol-link.html')
+  const appDirectory = __dirname.replace('app.asar', 'app.asar.unpacked')
+  const pagePath = path.join('file://', appDirectory, '../../sections/system/protocol-link.html')
   shell.openExternal(pagePath)
 })
