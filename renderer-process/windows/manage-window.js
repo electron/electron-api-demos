d8c9ebc50306177e262e5384fcda334bcb075d56
REDMOND\apavia <pavia.andres@gmail.com>
2016-09-26 13:08:22 -0700

    creating a windows focus & blur events section

--- renderer-process/windows/manage-window.js
+++ renderer-process/windows/manage-window.js
@@ -2,7 +2,6 @@ const BrowserWindow = require('electron').remote.BrowserWindow
 const path = require('path')
 
 const manageWindowBtn = document.getElementById('manage-window')
-const focusModalBtn = document.getElementById('focus-on-modal-window')
 let win
 
 manageWindowBtn.addEventListener('click', function (event) {
@@ -11,25 +10,15 @@ manageWindowBtn.addEventListener('click', function (event) {
   win.on('resize', updateReply)
   win.on('move', updateReply)
   win.on('close', function () { 
-    focusModalBtn.classList.add('no-display')
     win = null 
   })
   win.loadURL(modalPath)
   win.show()
-  showFocusBtn()
   function updateReply () {
     const manageWindowReply = document.getElementById('manage-window-reply')
     const message = `Size: ${win.getSize()} Position: ${win.getPosition()}`
-
     manageWindowReply.innerText = message
   } 
-  function showFocusBtn () {
-    focusModalBtn.classList.remove('no-display')
-  }
-})
-
-focusModalBtn.addEventListener('click', function (event) {
-   win.focus()
 })
 
 
---
+++ sections/windows/modal-toggle-visibility.html
@@ -0,0 +1,23 @@
+<style>
+body {
+  padding: 10px;
+  font-family: system, -apple-system, '.SFNSText-Regular', 'SF UI Text', 'Lucida Grande', 'Segoe UI', Ubuntu, Cantarell, sans-serif;
+  color: #fff;
+  background-color: #8aba87;
+  text-align: center;
+  font-size: 34px;
+}
+
+#close {
+  color: white;
+  opacity: 0.7;
+  position: absolute;
+  bottom: 20px;
+  left: 50%;
+  transform: translateX(-50%);
+  font-size: 12px;
+  text-decoration: none;
+}
+</style>
+<p>Click on the parent window to see how the "focus on demo" button appears.</p>
+<a id="close" href="javascript:window.close()">Close this Window</a>
