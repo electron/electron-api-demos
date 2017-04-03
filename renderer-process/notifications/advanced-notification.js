3a5b4febd8a5cfd09802caf6772821f0ecef507b
Simon Kaspersen <simon.kaspersen@gmail.com>
2017-04-03 23:15:18 +0200

    added notifications to the demo-application

---
+++ renderer-process/notifications/advanced-notification.js
@@ -0,0 +1,17 @@
+var path = require('path');
+
+const notification = 
+  {
+    title: "Notification with image",
+    body: "Short message plus a custom image",
+    icon: path.join(__dirname, '/../../assets/img/programming.png')
+  }
+const notificationButton = document.getElementById('advanced-noti')
+
+notificationButton.addEventListener('click', function () {
+  let myNotification = new Notification(notification.title, notification)
+
+	myNotification.onclick = () => {
+	  console.log('Notification clicked')
+	}
+})
---
+++ renderer-process/notifications/basic-notification.js
@@ -0,0 +1,14 @@
+const notification = 
+  {
+    title: "Basic Notification",
+    body: "Short message part"
+  }
+const notificationButton = document.getElementById('basic-noti')
+
+notificationButton.addEventListener('click', function () {
+  let myNotification = new Notification(notification.title, notification)
+
+  myNotification.onclick = () => {
+    console.log('Notification clicked')
+  }
+})
