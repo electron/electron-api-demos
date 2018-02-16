6e5e1a96daf3ceeca21bd6f31fbcc8e41ed387e4
Shelley Vohr <shelley.vohr@gmail.com>
2018-02-15 19:31:12 -0500

    finish updating main process files to es6

--- main-process/system/protocol-handler.js
+++ main-process/system/protocol-handler.js
@@ -1,8 +1,7 @@
-const app = require('electron').app
-const dialog = require('electron').dialog
+const {app, dialog} = require('electron')
 
 app.setAsDefaultProtocolClient('electron-api-demos')
 
-app.on('open-url', function (event, url) {
+app.on('open-url', (event, url) => {
   dialog.showErrorBox('Welcome Back', `You arrived from: ${url}`)
 })
