39b757501f45a0797413b8b0279a71c8d233d4ff
Celeste De Santiago <celestedaphne@gmail.com>
2018-03-22 14:08:57 -0700

    Add icon file extension

--- main-process/native-ui/drag/drag.js
+++ main-process/native-ui/drag/drag.js
@@ -2,7 +2,7 @@ const {ipcMain} = require('electron')
 const path = require('path')
 
 ipcMain.on('ondragstart', (event, filepath) => {
-  const iconName = 'codeIcon'
+  const iconName = 'codeIcon.png'
   event.sender.startDrag({
     file: filepath,
     icon: path.join(__dirname, iconName)
