872ffff4f723f0b9d2131a937521dc056765024d
Zeke Sikelianos <zeke@sikelianos.com>
2018-04-26 13:56:50 -0700

    use update-electron-app module

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
+require('update-electron-app')({
+  logger: require('electron-log')
+})
+
 const path = require('path')
 const glob = require('glob')
 const {app, BrowserWindow} = require('electron')
